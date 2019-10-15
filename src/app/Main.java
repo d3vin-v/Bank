96f7e9b2fa7f88358658fd75108d0f835c4eac01
d3vin-v <devinje01@gmail.com>
2019-10-15 12:18:03 -0500

    main menu 99%

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,45 +12,67 @@ public class Main {
 	{
 		Scanner scan = new Scanner(System.in);
 		Bank bank = new Bank("Bank of CPHS");
-		int ewbankAccountNo = bank.createAccount("Mr. Ewbank");
-		int howardAccountNo = bank.createAccount("Mr. Howard");
+//		int ewbankAccountNo = bank.createAccount("Mr. Ewbank");
+//		int howardAccountNo = bank.createAccount("Mr. Howard");
 		int menu;
-		String acctNum;
+		int acctNum;
 		int newAccountNo = 0;
 		int accountNumber;
 		
 		do {
-			System.out.println("1. Login with account number \n2. Create an account ");
+			System.out.println("  1. Login with account number \n  2. Create an account \n  3. Quit");
 			menu = scan.nextInt();
 			scan.nextLine();
 			
 			switch(menu){
 				case 1:
 					System.out.println("Please enter your account number: ");
-					acctNum = scan.nextLine();
+					acctNum = scan.nextInt();
+					if(acctNum != 0) { //TODO placeholder
+						System.out.println("Welcome back. \nCurrent balance: $" + bank.checkBalance(acctNum) +".00 \nWhat action would you like to perform?");
+						System.out.println("  1. Make a deposit \n  2. Make a withdrawal \n  3. Close account");
+						int menu2 = scan.nextInt();
+						switch(menu2) {
+							case 1:
+								System.out.println("How much would you like to deposit?");
+								int depositN = (int)scan.nextDouble();
+								bank.deposit(acctNum, depositN);
+								System.out.println("Deposited.\nCurrent balance: $" + bank.checkBalance(acctNum) + ".00");
+								break;
+							case 2:
+								System.out.println("How much would you like to withdraw?");
+								int withdrawN = (int)scan.nextDouble();
+								bank.withdraw(acctNum, withdrawN);
+								System.out.println("Withdrawn.\nCurrent balance: $" + bank.checkBalance(acctNum) + ".00");
+								break;
+						}
+					}else {
+						System.out.println("Error: Unknown account");
+					}
 					break;
 				case 2:
 					System.out.println("Please enter your name: ");
 					String acctName = scan.nextLine();
-					scan.nextLine();
 					newAccountNo = bank.createAccount(acctName);
 					System.out.println("Your account number is: " + bank.getAcctNum());
+					break;
+				case 3:
+					System.out.println("Goodbye.");
 			}
 			
-		}while(menu < 6);
+		}while(menu < 3);
 		
-		bank.deposit(ewbankAccountNo, 25);
-		bank.deposit(howardAccountNo, 75);
-		bank.deposit(newAccountNo, 100);
-
-		bank.checkBalance(ewbankAccountNo);
-		bank.checkBalance(howardAccountNo);
-		
-		bank.withdraw(ewbankAccountNo, 10);
-		bank.closeAccount(howardAccountNo);
-		
-		bank.checkBalance(ewbankAccountNo);
-		bank.checkBalance(howardAccountNo);
+//		bank.deposit(ewbankAccountNo, 25);
+//		bank.deposit(howardAccountNo, 75);
+//
+//		bank.checkBalance(ewbankAccountNo);
+//		bank.checkBalance(howardAccountNo);
+//		
+//		bank.withdraw(ewbankAccountNo, 10);
+//		bank.closeAccount(howardAccountNo);
+//		
+//		bank.checkBalance(ewbankAccountNo);
+//		bank.checkBalance(howardAccountNo);
 		
 		scan.close();
 	}
